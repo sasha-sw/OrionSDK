905bc852ade4e127e062f4c8988c9b88c2a62452
Oleksandr Vasyliev <oleksandr.vasyliev@solarwinds.com>
2019-04-15 14:22:08 +0300

    Playground

---
+++ Src/Playground/Program.cs
@@ -0,0 +1,13 @@
+﻿using SolarWinds.InformationService.Contract2;
+using System;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxy = new InfoServiceProxy(new Uri(""));
+        }
+    }
+}
